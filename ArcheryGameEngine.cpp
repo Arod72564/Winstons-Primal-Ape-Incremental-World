ad2cf5d0f366653d18d8ac49eaf683241d48404f
kylenvu <kylenvu@gmail.com>
2022-08-14 23:20:06 -0700

    Got rid of cout's

--- ArcheryGameEngine.cpp
+++ ArcheryGameEngine.cpp
@@ -102,7 +102,7 @@ void ArcheryGameEngine::initGame(){
 
     //Set initial view to player 1
     gameView->setCenter( archer1.archerTorsoSprite.getPosition() );
-    
+
 
 
 
@@ -116,7 +116,6 @@ void ArcheryGameEngine::update(){
         drag += std::complex<float>( (rand() % 201 - 100) / 100.f * 0.005f, (rand() % 201 - 100) / 100.f * 0.005f );
         wind_indicator.setScale(0.1, 0.1);
         wind_indicator.setRotation( std::arg(drag) * 180 / M_PI );
-        std::cout << "(" << std::abs(drag) << ", " << std::arg(drag) * 180 / M_PI << ")\n";
         turn_counter = (turn_counter + 1) % 3;
     }
 
@@ -159,7 +158,6 @@ void ArcheryGameEngine::update(){
         // std::cout << arrow1->arrow_velocity << " = (" << std::abs(arrow1->arrow_velocity) << ", " << std::arg(arrow1->arrow_velocity) * 180 / M_PI << ")" << std::endl;
         is_arrow_present = true;
         turn_counter = (turn_counter + 1) % 3;
-        std::cout << turn_counter << std::endl;
     } else {
         while (menuPtr->menuScreen->pollEvent(ev)) {
             switch (ev.type) {
@@ -251,7 +249,7 @@ void ArcheryGameEngine::calculateLine(Arrow* const arrow) {
 
 void ArcheryGameEngine::pan(sf::Sprite target) {
     pan_counter = 0;
-    
+
     panSpeed = std::complex<float> (target.getPosition().x - gameView->getCenter().x, target.getPosition().y - gameView->getCenter().y);
 
     panSpeed /= 100.f;
